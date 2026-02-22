e22661d48f59f2dab0f7c41e2233961fa5e7e3ba
Piyush Pandey <131640134+Piyush55236k@users.noreply.github.com>
2026-02-22 18:05:07 +0530

    Refactor projects list and add Weather Dashboard
    Updated project entries by modifying IDs and adding a new Weather Dashboard project.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,14 @@
 export const projects = [
-  {
+ {
     id: 1,
+    title: "Hirenova-A Hiring Platform",
+    image: "projects/HireNova.webp",
+    demo: "https://hirenova.vercel.app",
+    github: "https://github.com/Piyush55236k/HireNova",
+    tech: ["React", "Supabase", "Framer Motion","ShadcnUI"],
+    description: "Animated personal portfolio with scroll-based transitions."
+  }, {
+    id: 2,
     title: "Taskizo Freelance Platform",
     image: "projects/Taskizo.webp",
     demo: "https://taskizo-website.vercel.app",
@@ -9,25 +17,7 @@ export const projects = [
     description: "A MERN-based freelance marketplace with real-time bidding and authentication."
   },
   {
-    id: 2,
-    title: "Weather Dashboard",
-    image: "projects/Weather.webp",
-    demo: "https://countryweatherchecker-ten.vercel.app",
-    github: "https://github.com/Piyush55236k/weather-detect",
-    tech: ["React", "API Integration"],
-    description: "Responsive weather application using OpenWeatherMap API."
-  },
-  {
     id: 3,
-    title: "Hirenova-A Hiring Platform",
-    image: "projects/HireNova.webp",
-    demo: "https://hirenova.vercel.app",
-    github: "https://github.com/Piyush55236k/HireNova",
-    tech: ["React", "Supabase", "Framer Motion","ShadcnUI"],
-    description: "Animated personal portfolio with scroll-based transitions."
-  },
-  {
-    id: 4,
     title: "Agro Mitra - SIH Project",
     image: "projects/Agro Mitra.webp",
     demo: "https://newsih-gtmo.vercel.app",
@@ -36,7 +26,7 @@ export const projects = [
     description: "Responsive e-commerce interface design."
   },
   {
-    id: 5,
+    id: 4,
     title: "Landing Page Design",
     image: "projects/Sundown.webp",
     demo: "https://sundown-web-page.vercel.app",
@@ -45,12 +35,21 @@ export const projects = [
     description: "Modern responsive landing page."
   },
   {
-    id: 6,
+    id: 5,
     title: "TextUtils-Play with text",
     image: "projects/TextUtils.webp",
     demo: "https://text-utils-blue-delta.vercel.app",
     github: "https://github.com/Piyush55236k/TextUtils",
     tech: ["React"],
     description: "Interactive UI experiment."
+  },
+   {
+    id: 6,
+    title: "Weather Dashboard",
+    image: "projects/Weather.webp",
+    demo: "https://countryweatherchecker-ten.vercel.app",
+    github: "https://github.com/Piyush55236k/weather-detect",
+    tech: ["React", "API Integration"],
+    description: "Responsive weather application using OpenWeatherMap API."
   }
 ];
